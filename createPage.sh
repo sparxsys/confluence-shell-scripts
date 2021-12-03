797c66d5c87e4fb5d99c41843e16fca3971e391c
Ravi Sagar <ravisagar@gmail.com>
2021-12-03 16:39:19 +0000

    Create a Confluence page

---
+++ createPage.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+curl -i -X POST \
+     -H "Authorization:Basic $JIRATUTORIAL_AUTH" \
+     -H "Content-Type:application/json" \
+     -H "X-Atlassian-Token:no-check" \
+     "https://jiratutorial.atlassian.net/wiki/rest/api/content/" \
+     -d '{
+     	    "space": {
+			"key": "MS" 
+	    },
+	    "type": "page",
+	    "title": "Page created from Shell Script - 2",
+	    "body": {
+	    	    "storage": {
+		    	       "value": "This is my page content",
+			       "representation": "wiki"
+		  }
+	     }
+      }'
